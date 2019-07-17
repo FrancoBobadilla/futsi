c84ee060be056e3aeda6188bfce1189fc7860db8
FrancoBobadilla <1709673@ucc.edu.ar>
2019-07-17 18:39:46 -0300

    se agrego toda la stuff de firebase

---
+++ functions/test/index.test.js
@@ -0,0 +1,5 @@
+const test = require('firebase-functions-test')({
+  databaseURL: 'https://futsi-8d92a.firebaseio.com',
+  storageBucket: 'futsi-8d92a.appspot.com',
+  projectId: 'futsi-8d92a',
+}, '../serviceAccountKey.json');
